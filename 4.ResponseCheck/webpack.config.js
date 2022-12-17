987493f2969177e8da64577f2cb7e4f6ff79001b
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2022-12-17 22:14:36 +0900

    ResponseCheck

---
+++ 4.ResponseCheck/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpackMode = process.env.NODE_ENV || "development";
+
+module.exports = {
+  name: 'response-check',
+  mode: webpackMode,
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: {
+    app: './src/client',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {browsers: ['> 5% in KR', 'last 2 chrome versions']},
+            debug: true,
+          }],
+          '@babel/preset-react',
+        ],
+      },
+      exclude: path.join(__dirname, 'node_modules'),
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+  ],
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].min.js",
+  },
+  devServer: {
+    static: { directory: path.resolve(__dirname) },
+    hot: true,
+    port: 8084,
+    allowedHosts: "all",
+    host: "0.0.0.0",
+  }
+};
